430f0db2edd852126376e5df08d82cc1ceb4141d
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-10-12 21:26:13 +0600

    added final part

--- src/components/Quize.js
+++ src/components/Quize.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Quize = ({quize}) => {
     const {question, options} = quize;
     return (
-        <div lassName='shadow-xl p-10 m-14 rounded-lg'>
-        <div className='flex'>
+        <div lassName='shadow-xl p-10 m-20 rounded-lg'>
             <h1 className='text-xl text-yellow-500'> Quize: {question}</h1>
-            <FontAwesomeIcon icon={faArrowRight} />
-        </div>
-            <div>
             {
                 options.map(option => <ul className=''>
                         <div className='flex border-solid border-2 border-indigo-400 rounded-lg p-5 m-5'>
@@ -20,8 +14,6 @@ const Quize = ({quize}) => {
                     </ul>
                 )
             }
-            </div>
-            
         </div>
     );
 };
